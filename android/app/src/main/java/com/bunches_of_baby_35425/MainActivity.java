0d022b9e8504a0ae43bae17580bd0296136168cc
Crowdbotics <team@crowdbotics.com>
2022-05-24 18:08:21 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/bunches_of_baby_35425/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bunches_of_baby_35425;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "bunches_of_baby_35425";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bunches_of_baby_35425",
+  url: "https://bunches_of_baby_35425.botics.co",
+  api: "https://bunches_of_baby_35425.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
